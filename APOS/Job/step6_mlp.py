52e01cf177e03a505cb4071fd230245ccc83a992
lyoshiwo <lyoshiwo.com>
2016-12-03 12:55:41 +0800

    data数据提交

--- APOS/Job/step6_mlp.py
+++ APOS/Job/step6_mlp.py
@@ -9,6 +9,8 @@ from keras.optimizers import SGD, Adam, RMSprop
 from sklearn.cross_validation import train_test_split
 from keras.layers.embeddings import Embedding
 from sklearn import preprocessing
+import numpy as np
+
 [x, y, _, _] = pd.read_pickle('pickle/manual_position_size_salary.pkl')
 # enc = preprocessing.OneHotEncoder()
 # enc.fit(x)
@@ -17,7 +19,9 @@ y_ = np_utils.to_categorical(y, len(set(y)))
 X_train, X_test, Y_train, Y_test = train_test_split(x, y_, test_size=0.33, random_state=713)
 
 
-# 38 epoch,0.456
+#
+#
+# # 38 epoch,0.456
 def mlp():
     # from keras.regularizers import l1
     model = Sequential()
@@ -44,16 +48,46 @@ def mlp():
 
 
 def mlp2():
+    from sklearn import preprocessing
+    import pandas as pd
+    import numpy as np
     [x, _, _, _] = pd.read_pickle('pickle/manual_position_size_salary.pkl')
     enc = preprocessing.OneHotEncoder()
     enc.fit(x)
-    feature_num = len(enc.active_features_)
     one_hot_x = enc.transform(x)
     indexs = one_hot_x.indices
     blocks = one_hot_x.indptr
     print len(indexs)
     print len(blocks)
-    # all_data = np.zeros((x.shape[0],feature_num))
+    all_data = np.zeros((one_hot_x.shape[0], one_hot_x.shape[1]))
+    print blocks[-1]
+    print indexs[blocks[-1]:]
+    for b in range(one_hot_x.shape[0]):
+        for v in indexs[blocks[b]:blocks[b + 1]]:
+            all_data[b][v] = 1
+    model = Sequential()
+    x = all_data
+    X_train, X_test, Y_train, Y_test = train_test_split(x, y_, test_size=0.33, random_state=713)
+    from keras.regularizers import l1
+    model.add(Dense(input_dim=x.shape[0], output_dim=len(set(y)), W_regularizer=l1(0.01)))
+    # model.add(Dense(128))
+    # model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(len(set(y))))
+    model.add(Activation('softmax'))
+    rms = RMSprop()
+    model.compile(loss='categorical_crossentropy', optimizer=rms, metrics=["accuracy"])
+    batch_size = 1024
+    nb_epoch = 1000
+    model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
+              validation_data=(X_test, Y_test))
+    model.summary()
+    score = model.evaluate(X_test, Y_test)
+    print('Test score:', score[0])
+    print('Test accuracy:', score[1])
+
+
+# all_data = np.zeros((x.shape[0],feature_num))
 
 
 mlp2()
